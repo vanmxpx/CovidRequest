f21cb663ded3244ebde0c00f36a841a7fca50560
Hanna <HannaBuzovskaya@github.com>
2021-05-01 13:14:04 +0300

    Добавлена функция вылогинивания

--- client/src/app/components/main/cov-nav-header/cov-nav-header.component.ts
+++ client/src/app/components/main/cov-nav-header/cov-nav-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router } from '@angular/router';
 import { Profile } from '@cov/shared/models/BL/profile';
 import { AuthService } from '@cov/shared/services';
 import { ProfileRepository } from '@cov/shared/services/repositories/profile.repository';
@@ -10,13 +11,26 @@ import { ProfileRepository } from '@cov/shared/services/repositories/profile.rep
 })
 export class CovNavHeaderComponent implements OnInit {
   profileImage: string = null;
+  model: any = {};
+  loading = false;
+  returnUrl: string;
+
   constructor(
     private profileRepository: ProfileRepository,
-    private authService: AuthService
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   async ngOnInit() {
     this.profileImage = await this.profileRepository.getUserPhoto(this.authService.currentUser.personalInfoRef).toPromise();
+   
+    this.authService.logout();
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
+  login () {
+    this.loading = true;
+        this.authService.loginByGoogle;
+  }
 }
